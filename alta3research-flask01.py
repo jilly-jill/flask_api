1793b50ab6391bb39d7f9f2fcfe79f07d4a69af1
Jillian Sechrest <93422213+jilly-jill@users.noreply.github.com>
2022-04-15 18:54:21 -0400

    Update alta3research-flask01.py

--- alta3research-flask01.py
+++ alta3research-flask01.py
@@ -1,18 +1,24 @@
-#!/usr/bin/python3
-  2 from flask import Flask
-  3 from flask import render_template
-  4 
-  5 
-  6 app = Flask(__name__)
-  7 
-  8 
-  9 @app.route('/')
- 10 def index():
- 11     return jsonify(mutant_data)
- 12 
- 13  mutant_data = {
- 14         'name':'Magneto',
- 15         'powers':['Magnetism','Intelligence','Charisma'],
- 16         'roles':{
- 17             'Quiet Counsel':['Representative','Founder'],
- 18             'Brotherhood of Evil Mutants':['Patriarch', 'Founder']
+#!/usr/bin/python
+from flask import Flask
+from flask import render_template
+  
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return jsonify(mutant_data)
+mutant_data = {
+         'name':'Magneto',
+         'powers':['Magnetism','Intelligence','Charisma'],
+         'roles':{
+                 'Quiet Counsel':['Representative','Founder'],
+                 'Brotherhood of Evil Mutants':['Patriarch', 'Founder']
+          }
+ }
+@app.route('/multi' methods=['GET', 'POST'])
+def multi():
+  
+  return render_template('index.html', player=mutant_data)
+  
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=2224)
